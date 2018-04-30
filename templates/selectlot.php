12e74b5218f14253550bc2e1f6860c24e9fc338a
stein christensen <steinc95@gmail.com>
2018-04-30 17:39:47 -0600

    db config

--- templates/selectlot.php
+++ templates/selectlot.php
@@ -2,7 +2,7 @@
 $search = Lot::getAllLots(null);
 $grid="";
 while ($row = $search->fetch_assoc()){
-	$grid.='<h2 style="display: inline-block;">'.$row["name"].'</h2><a href="'.SERVERROOT.'/add.php?LID='.$row['ID'].'/"><div class="confirmButton" style="display: inline-block;">Add Ticket</div></a>';
+	$grid.='<h2 style="display: inline-block;">'.$row["name"].'</h2><a href="'.SERVERROOT.'/add.php?LID='.$row['ID'].'/"><div class="confirmButton" style="display: inline-block;">Add Ticket</div></a><br>';
 }
 
 echo $grid;
