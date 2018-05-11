7c48bbb6f9a56839b21ea32f7272ce8c6764a15d
stein christensen <steinc95@gmail.com>
2018-05-11 15:11:41 -0600

    #50

--- templates/head.php
+++ templates/head.php
@@ -11,7 +11,7 @@
     <meta name="author" content="">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <title>Parking Collection Systems</title>
+    <title>Parking Collection</title>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
