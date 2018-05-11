efa40036b61c7f837c1eb41499eff3bf7ab3e03d
stein christensen <steinc95@gmail.com>
2018-05-11 13:57:20 -0600

    #50

--- templates/checkform.php
+++ templates/checkform.php
@@ -8,14 +8,11 @@
 <?php
 	if(isset($_POST['submit'])){
 		$violationCount = Warning::getNumberOfWarningsByPlate($_POST['licensePlate'], null) + Ticket::getNumberOfTicketsByPlate($_POST['licensePlate'], null);
-		if($violationCount >0){
+
 			echo "Number of Warnings: ".Warning::getNumberOfWarningsByPlate($_POST['licensePlate'], null)."<br>";
 			echo "Number of Tickets: ".Ticket::getNumberOfTicketsByPlate($_POST['licensePlate'], null)."<br><br>";
 			Includes::getWarningRecord($_POST['licensePlate']);
 			Includes::getTicketRecord($_POST['licensePlate']);
 			Includes::getTicketOrWarn();
-			
-		}else{
-			echo "ticket ID doesn't exist";
-		}
+
 }
